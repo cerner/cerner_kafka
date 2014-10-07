69bec2de4bedf134a658380d0b3b33cfa87e8b8a
Bryan Baugher <bryan.baugher@cerner.com>
2014-10-07 14:27:11 -0500

    Released 1.0.0 and bumped version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,4 @@ maintainer        "Cerner Corp."
 maintainer_email  "Bryan.Baugher@cerner.com"
 license           "Apache 2.0"
 description       "Installs and configures a Kafka"
-version           "1.0.0"
-
-supports "centos"
-supports "ubuntu"
-
-depends "java"
-depends "ulimit"
+version           '1.1.0'
