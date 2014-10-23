91a5f975ab156482c7f9975126a23adf02813839
Bryan Baugher <bryan.baugher@cerner.com>
2014-10-23 07:13:58 -0500

    Added cookbook dependencies to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,6 @@ maintainer_email  "Bryan.Baugher@cerner.com"
 license           "Apache 2.0"
 description       "Installs and configures a Kafka"
 version           '1.1.0'
+
+depends 'java'
+depends 'ulimit'
