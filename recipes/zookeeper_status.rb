291bdd5be6aef4048e3e611e599b0dc006d255d8
Pradeep Bhadani <pradeep.bhadani@bigdatapartnership.com>
2014-10-29 15:01:14 +0000

    Change way to check Zookeeper service

--- recipes/zookeeper_status.rb
+++ recipes/zookeeper_status.rb
@@ -2,9 +2,19 @@
 # Cookbook Name:: cerner_kafka
 # Recipe:: zookeeper_status
 
-
+<<-comment
 execute 'check zookeeper' do
   action :run
   command " nc -z #{node["kafka"]["zookeepers"].first} 2181 "
   returns [0]
+end
+comment
+
+
+
+ruby_block "Zookeeper Status Check" do
+    block do
+      fail "Zookeeper not Running"
+    end
+    not_if  "nc -z #{node['kafka']['zookeepers'].first} 2181" 
 end
