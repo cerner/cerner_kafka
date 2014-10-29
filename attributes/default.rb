a5655c9c43387608360f57775811ca7075b02d2b
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2014-10-29 14:29:42 +0000

    Add path for kafka service stdout and stderr

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,6 +12,9 @@ default["kafka"]["download_url"] = "https://archive.apache.org/dist/kafka"
 default["kafka"]["base_dir"]  = "/opt"
 default["kafka"]["log_dir"] = "/var/log/kafka"
 
+default["kafka"]["service"]["stdout"] = "/dev/null"
+default["kafka"]["service"]["stderr"] = "/dev/null"
+
 # These are required to be supplied by the consumer so setting to nil
 default["kafka"]["brokers"] = nil
 default["kafka"]["zookeepers"] = nil
