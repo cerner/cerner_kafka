d87e68d67f7d961183991e95a39b17ddde90533a
Austin Heiman <atheimanksu@gmail.com>
2016-04-21 10:14:47 -0500

    updated contributors, added issues and source url for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer        "Cerner Corp."
 maintainer_email  "Bryan.Baugher@cerner.com"
 license           "Apache 2.0"
 description       "Installs and configures a Kafka"
+issues_url        'https://github.com/cerner/cerner_kafka/issues'
+source_url        'https://github.com/cerner/cerner_kafka'
 
 depends 'java'
 depends 'ulimit'
